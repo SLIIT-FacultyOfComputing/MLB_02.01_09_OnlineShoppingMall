909509e1ad3000185e8c01490adbc391215e4c6c
IT21080258 <87359151+IT21080258@users.noreply.github.com>
2021-10-15 19:42:11 +0530

    Delete OOC-assingment-2 (1) directory

--- OOC-assingment-2 (1)/supplier.h
+++
@@ -1,9 +0,0 @@
-/*IT21080258 GamageK.M.
-MLB_02.01_09*/
-class supplier{
-  private:
-    int supplierID;
-    char supplierName[100];
-  public:
-    void setsupplierDetails(int supID, const char supName[]);  
-};
